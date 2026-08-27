d1bc27d2a608f640300b3bbdcf476682190b44e2
Sebastian Wauer <info@sport-voice.de>
2026-08-27 20:37:30 +0200

    feat: add subscription expiry and bulk rollout

---
+++ migrations/048_changelog_2_2_2.sql
@@ -0,0 +1,12 @@
+-- CMS Release 2.2.2 - Update-Zeitraum in der Sidebar
+
+SET NAMES utf8mb4;
+
+INSERT INTO changelogs (version, type, module_key, content_md, released_at)
+VALUES (
+    '2.2.2',
+    'cms',
+    NULL,
+    '## Version 2.2.2\n\n### Update-Zeitraum\n\n- In der Sidebar wird unterhalb der CMS-Version angezeigt, bis wann Updates verfügbar sind. Bei einem unbefristeten Abonnement wird „unbegrenzt“ angezeigt.',
+    '2026-08-27 00:00:00'
+);
